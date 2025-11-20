8f3efa67758b18892c5067b5b6e1f23d0c0e8c7a
zappap <51238007+zappap@users.noreply.github.com>
2025-11-20 23:43:41 +0300

    Refactor and enhance PDF to XLS conversion logic

--- app-sami.py
+++ app-sami.py
@@ -11,7 +11,7 @@ from xlutils.copy import copy
 # Page Config
 st.set_page_config(page_title="Sami PDF to XLS", layout="centered")
 
-st.title("🚛 🚛 🚛 Sami Plaka Sistemi 🚛 🚛 🚛")
+st.title("🚛 🚛 Sami Plaka Sistemi 🚛 🚛")
 
 # --- 1. User Inputs ---
 direction_choice = st.radio("İşlem Türü:", ('Çıkış', 'Giriş'))
@@ -129,4 +129,5 @@ if st.button("Dönüştür ve İndir"):
         except Exception as e:
             st.error(f"Bir hata oluştu: {e}")
             st.exception(e)
+
 
